8be9860399c260f1ee47ac6508498d0f36235ba8
Keirnan Fuentes Urrea <keirnanfu@gmail.com>
2023-04-03 23:01:26 -0400

    Agregar archivos de proyecto.

---
+++ Restaurante/InicioSesion.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Restaurante
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.TxtContrasena = new System.Windows.Forms.TextBox();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(199, 143);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // TxtContrasena
+            // 
+            this.TxtContrasena.Location = new System.Drawing.Point(199, 243);
+            this.TxtContrasena.Name = "TxtContrasena";
+            this.TxtContrasena.PasswordChar = '*';
+            this.TxtContrasena.Size = new System.Drawing.Size(100, 20);
+            this.TxtContrasena.TabIndex = 2;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(105, 297);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnInicio.TabIndex = 3;
+            this.btnInicio.Text = "Entrar";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(317, 297);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 376);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.TxtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox TxtContrasena;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Restaurante/InicioSesion.cs
@@ -0,0 +1,84 @@
+﻿using Intercom.Core;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace Restaurante
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        OracleConnection conexion = new OracleConnection("DATA SOURCE = ORCL ; PASSWORD = admin ; USER ID = Admin");
+        
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+
+            OracleCommand comando = new OracleCommand("select * from usuarios where nombre_usuario = :nombre_usuario and contrasena = :contrasena", conexion);
+
+            comando.Parameters.Add(":usuario", txtUsuario.Text);
+            comando.Parameters.Add("contrasena", TxtContrasena.Text);
+
+            OracleDataReader lector = comando.ExecuteReader();
+
+
+            if (lector.Read())
+            {
+                string rol = lector.GetString(2); 
+
+                if (rol == "admin")
+                {
+                    
+                    MenuAdmin menuAdmin = new MenuAdmin();
+                    menuAdmin.Show();
+                    this.Hide(); 
+                }
+                else
+                {
+
+                    InicioSesion inicioSesion = new InicioSesion();
+                    inicioSesion.Show();
+                    this.Hide(); 
+                }
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
